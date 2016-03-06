005e82b47ea4bde1ffad02a73527537e784cb088
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-03-06 19:40:00 +0400

    move to infrajs

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <h1>Timezone</h1>
 <?php
 	require_once('Timezone.php');
-	$zone=itlife\timezone\Timezone::detect();
+	$zone=\infrajs\timezone\Timezone::detect();
 	if ($zone) {
 		echo $zone;
 	} else {
 		echo 'Временная зона неопределена. Вероятней всего это первое посещение страницы. И страницу нужно обновить.';
 	}
 ?>
-<p><a href="https://github.com/akiyatkin/timezone">github</a></p>
+<p>GitHub <a href="https://github.com/infrajs/timezone">infrajs/timezone</a></p>
