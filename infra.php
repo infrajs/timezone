e90c79df9c07edac2c90b305fc898c8016278dc2
Anton Kiyatkin <axlbant@sent.com>
2016-03-06 19:17:13 +0400

    Update infra.php

--- infra.php
+++ infra.php
@@ -1,17 +1,10 @@
 <?php
-namespace itlife\timezone;
+namespace infrajs\timezone;
 
-global $infra;
-infra_when($infra, 'oninit', function () {
-	$zone=Timezone::detect();
-	if ($zone) {
-		date_default_timezone_set($zone);
-		@header('Infra-Timezone:'.$zone);
-	} else {
-		@header('Infra-Timezone:false');
-	}
-});
-infra_when($infra,'onjs',function(){
-	global $infra;
-	$infra['js'] .= $infra['require']('*timezone/timezone.js');
-});
+$zone = Timezone::detect();
+if ($zone) {
+	date_default_timezone_set($zone);
+	header('Infra-Timezone:'.$zone);
+} else {
+	header('Infra-Timezone:false');
+}
